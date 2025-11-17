986ed8dfda2957e0a3705c80a936f40d26c5f490
vitalfeed <vitalfeed020@gmail.com>
2025-11-17 15:12:20 +0100

    Update CorsConfig.java

--- FormulaireVeterinaire/src/main/java/com/veterinaire/formulaireveterinaire/Config/CorsConfig.java
+++ FormulaireVeterinaire/src/main/java/com/veterinaire/formulaireveterinaire/Config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:4200", "https://vercelfront.vercel.app/") // Angular dev server
+                        .allowedOrigins("https://vercelfront-8zw2nbsvs-vital-feeds-projects.vercel.app", "https://vercelfront.vercel.app/") // Angular dev server
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true); // Autorise les cookies/tokens
